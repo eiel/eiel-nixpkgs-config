5ffdf5e063774d2e521b0495483b37f294139261
Tomohiko Himura <eiel@TomohikonoMacBook-Pro.local>
2023-02-05 13:01:04 +0900

    initial commit

---
+++ darwin-configuration.nix
@@ -0,0 +1,35 @@
+{ config, pkgs, ... }:
+
+{
+  # List packages installed in system profile. To search by name, run:
+  # $ nix-env -qaP | grep wget
+  environment.systemPackages =
+    [ 
+      pkgs.vim
+      pkgs.git
+    ];
+
+  # Use a custom configuration.nix location.
+  # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
+  # environment.darwinConfig = "$HOME/.config/nixpkgs/darwin/configuration.nix";
+
+  # Auto upgrade nix package and the daemon service.
+  services.nix-daemon.enable = true;
+  # nix.package = pkgs.nix;
+
+  # Create /etc/zshrc that loads the nix-darwin environment.
+  programs.zsh.enable = true;  # default shell on catalina
+  programs.fish.enable = true;
+
+  homebrew.enable = true;
+  homebrew.casks = [
+      "1password"
+      "karabiner-elements"
+    ];
+
+  system.defaults.NSGlobalDomain.InitialKeyRepeat = 12;
+  system.defaults.NSGlobalDomain.KeyRepeat = 1;
+  # Used for backwards compatibility, please read the changelog before changing.
+  # $ darwin-rebuild changelog
+  system.stateVersion = 4;
+}

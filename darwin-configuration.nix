9dd7aaa559b4c6b5dbd43ed285be99cfd9357967
HIMURA Tomohiko <eiel.hal@gmail.com>
2023-02-12 17:15:27 +0900

    fmt: nix fmt

--- darwin-configuration.nix
+++ darwin-configuration.nix
@@ -18,7 +18,7 @@
 
   # List packages installed in system profile. To search by name, run:
   # $ nix-env -qaP | grep wget
-  environment.systemPackages = [];
+  environment.systemPackages = [ ];
 
   # Use a custom configuration.nix location.
   # $ darwin-rebuild switch -I darwin-config=$HOME/.config/nixpkgs/darwin/configuration.nix
@@ -33,7 +33,7 @@
   };
 
   # Create /etc/zshrc that loads the nix-darwin environment.
-  programs.zsh.enable = true;  # default shell on catalina
+  programs.zsh.enable = true; # default shell on catalina
   # programs.fish.enable = true;  # default shell on catalina
 
   homebrew.enable = true;
